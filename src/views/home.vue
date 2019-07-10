2db66e29994418a4b73d096b024e3b76846b4a6c
WHEATsky <373189138@qq.com>
2019-07-10 21:50:19 +0800

    wei-1

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,13 +2,14 @@
   <add-search></add-search>
 </template>
 
+
 <script>
   import AddSearch from '../components/home/AddSearch'
 export default {
     components:{
       AddSearch
     }
-};
+}
 </script>
 
 <style scoped>
