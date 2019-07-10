7023a812c53f3838b27850ec626352eb3c42311f
yaoyaoqi117 <58429744@qq.com>
2019-07-10 21:54:18 +0800

    navbar组件

--- src/components/home/Navbar.vue
+++ src/components/home/Navbar.vue
@@ -1,10 +1,67 @@
 <template>
-  <div></div>
+  <div id="min">
+    <div class="min-top">
+      <div class="borders"></div>
+      <div class="min-top-two">
+        <span>推荐商家</span>
+      </div>
+      <div class="borders"></div>
+    </div>
+    <div class="min-bottom">
+      <div>
+        <span>综合排序</span>
+        <i class="fa fa-caret-down"></i>
+      </div>
+      <div>
+        <span>距离最近</span>
+      </div>
+      <div>
+        <span>品质联盟</span>
+      </div>
+      <div>
+        <span>筛选</span>
+        <i class="fa fa-filter"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Navbar"
+};
 </script>
 
-<style>
+<style scoped>
+#min {
+  width: 90%;
+  height: 3.75rem;
+  margin: 0 auto;
+  line-height: 3.75rem;
+}
+#min .min-top {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+#min .borders {
+  width: 1.25rem;
+  border-top: 1px solid black;
+}
+#min .min-top-two {
+  width: 25%;
+  font-size: 1.0625rem;
+  text-align: center;
+}
+#min .min-bottom {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
+#min .min-bottom div {
+  color: #666;
+  font-weight: 700;
+  /* font-size: 0.9rem; */
+}
 </style>
