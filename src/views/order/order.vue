05e2c941643248b1c012f56b26611e728aa14815
abin12 <1259446122@qq.com>
2019-07-12 16:26:45 +0800

    abin12xia

---
+++ src/views/order/order.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <OrderTime></OrderTime>
+  </div>
+</template>
+
+<script>
+
+import OrderTime from "../../components/ele-order/order-time"
+  export default {
+    computed:{
+
+    },
+    components:{
+        OrderTime,
+    }
+  };
+</script>
+
+<style lang="scss">
+
+</style>
