a4190bb9435f7bb19d6bb3cb47725772413ceb8c
WHEATsky <373189138@qq.com>
2019-07-13 18:18:48 +0800

    wei7-13

--- src/router/index.js
+++ src/router/index.js
@@ -71,13 +71,13 @@ const router = new Router({
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  const isLogin = localStorage.ele_login ? true : false;
-  if (to.path == "/phoneLogin") {
-    next();
-  } else {
-    //是否登录状态下
-    isLogin ? next() : next("/phoneLogin");
-  }
-});
+// router.beforeEach((to, from, next) => {
+//   const isLogin = localStorage.ele_login ? true : false;
+//   if (to.path == "/phoneLogin") {
+//     next();
+//   } else {
+//     //是否登录状态下
+//     isLogin ? next() : next("/phoneLogin");
+//   }
+// });
 export default router;
